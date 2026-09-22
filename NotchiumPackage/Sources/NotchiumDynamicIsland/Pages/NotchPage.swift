127b983dbe99a00326bce38de7f1f3e6ee31a991
Marcus-Yu <marcusyuca@gmail.com>
2026-09-22 14:20:07 -0400

    Update NotchPage.swift
    Commit the isolated changes for NotchiumPackage/Sources/NotchiumDynamicIsland/Pages/NotchPage.swift.

--- NotchiumPackage/Sources/NotchiumDynamicIsland/Pages/NotchPage.swift
+++ NotchiumPackage/Sources/NotchiumDynamicIsland/Pages/NotchPage.swift
@@ -1,11 +1,12 @@
 public enum NotchPage: String, CaseIterable, Identifiable, Sendable {
-    case music, calendar
+    case music, calendar, audio
     public var id: String { rawValue }
 
     public var title: String {
         switch self {
         case .music: "Music"
         case .calendar: "Calendar"
+        case .audio: "Audio"
         }
     }
 
@@ -13,6 +14,7 @@ public enum NotchPage: String, CaseIterable, Identifiable, Sendable {
         switch self {
         case .music: "music.note"
         case .calendar: "calendar"
+        case .audio: "speaker.wave.2"
         }
     }
 }
