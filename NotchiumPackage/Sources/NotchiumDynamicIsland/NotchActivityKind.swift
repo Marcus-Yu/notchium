cc2ac79ff694018de62334963e389096bd4b7d5d
Marcus-Yu <marcusyuca@gmail.com>
2026-09-24 15:12:19 -0400

    Update NotchActivityKind.swift
    Commit the changes to NotchiumPackage/Sources/NotchiumDynamicIsland/NotchActivityKind.swift separately.

--- NotchiumPackage/Sources/NotchiumDynamicIsland/NotchActivityKind.swift
+++ NotchiumPackage/Sources/NotchiumDynamicIsland/NotchActivityKind.swift
@@ -1,18 +1,30 @@
-public enum NotchActivityKind: String, CaseIterable, Sendable {
+public enum NotchActivityKind: String, CaseIterable, Hashable, Sendable {
     case media, charging, battery, audioDevice, systemHUD, calendar
     case meeting, download, screenshot, clipboard, focus, notification
 
-    public var priority: Int {
+    public var priority: NotchActivityPriority {
         switch self {
-        case .systemHUD: 10
-        case .media: 20
-        case .charging, .audioDevice: 30
-        case .download, .screenshot: 40
-        case .calendar: 15
-        case .focus: 60
-        case .battery: 70
-        case .meeting, .clipboard: 80
-        case .notification: 100
+        case .media, .systemHUD: .low
+        case .charging, .audioDevice, .download, .screenshot, .calendar: .medium
+        case .focus, .battery, .meeting, .clipboard: .high
+        case .notification: .critical
+        }
+    }
+
+    public var family: NotchActivityFamily {
+        switch self {
+        case .media: .media
+        case .calendar, .meeting: .calendar
+        case .audioDevice, .systemHUD: .audio
+        default: .other(self)
+        }
+    }
+
+    public var defaultPresentationStyle: NotchActivityPresentationStyle {
+        switch self {
+        case .media: .mediaSides
+        case .audioDevice, .systemHUD: .compactHUD
+        default: .downwardBanner
         }
     }
 }
