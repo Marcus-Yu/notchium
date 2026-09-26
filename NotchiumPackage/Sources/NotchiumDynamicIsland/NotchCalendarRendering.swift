4217e7087c3327c3a3a8a46156a6e64727774a3c
Marcus-Yu <marcusyuca@gmail.com>
2026-09-25 23:07:23 -0400

    Update NotchCalendarRendering.swift

--- NotchiumPackage/Sources/NotchiumDynamicIsland/NotchCalendarRendering.swift
+++ NotchiumPackage/Sources/NotchiumDynamicIsland/NotchCalendarRendering.swift
@@ -5,9 +5,14 @@ import SwiftUI
     var reminderVisible: Bool { get }
     func reminderBanner(action: @escaping @MainActor () -> Void) -> AnyView
     func setReminderHovered(_ hovered: Bool)
+    func homeCalendar(openCalendar: @escaping @MainActor () -> Void) -> AnyView
     func expandedCalendar() -> AnyView
 }
 
 extension EnvironmentValues {
     @Entry public var notchCalendarPageVisible = true
+}
+
+public extension NotchCalendarRendering {
+    func homeCalendar(openCalendar: @escaping @MainActor () -> Void) -> AnyView { AnyView(Button("Open Calendar", action: openCalendar).buttonStyle(.plain)) }
 }
